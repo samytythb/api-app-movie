67c91027a54d7ab3eedb3c447481f26657b30f89
Le Minh Duc <79952214+samytythb@users.noreply.github.com>
2023-07-04 14:52:21 +0700

    first commit

---
+++ src/main/java/dev/leo/movies/MovieRepository.java
@@ -0,0 +1,13 @@
+package dev.leo.movies;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface MovieRepository extends MongoRepository<Movie, ObjectId> {
+    Optional<Movie> findMovieByImdbId(String imdbId);
+//    Optional<Movie> findByTitle(String title);
+}
